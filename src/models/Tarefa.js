afc18ed467180e8c8d0ef2d369540011ba5d5edd
matheus griza <matheusgriza@sou.faccat.br>
2025-09-03 20:41:20 -0300

    feat: Implemented model Tarefa

---
+++ src/models/Tarefa.js
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/db.js';
+
+const Tarefa = sequelize.define(
+  'Tarefa',
+  {
+    titulo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    concluida: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: 'tarefas',
+  }
+);
+
+export { Tarefa };
