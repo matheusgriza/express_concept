468c1133bf8a4f965ddd0e047d31fb97b447bfe7
matheus griza <matheusgriza@sou.faccat.br>
2025-09-28 19:13:27 -0300

    feat: installed required libs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes/tasks.js';
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+  })
+);
+
+app.get('/dados', (req, res) => {
+  res.json({ message: 'CORS working properly' });
+});
 
 app.use('/tasks', router);
 
@@ -15,5 +24,3 @@ app.listen(PORT, (req, res) => {
 app.get('/', (req, res) => {
   res.send('Welcome to my backend');
 });
-
-
